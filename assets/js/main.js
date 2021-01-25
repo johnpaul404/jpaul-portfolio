fd4e657f8661c1561562342e783e65237203e24a
John Paul Manquiquis <72723962+johnpaul404@users.noreply.github.com>
2021-01-25 13:05:51 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Strata by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -92,6 +87,23 @@
 				});
 
 			}
+
+						var btn = $('#top');
+
+			$(window).scroll(function() {
+			if ($(window).scrollTop() > 300) {
+				btn.addClass('show');
+			} else {
+				btn.removeClass('show');
+			}
+			});
+
+			btn.on('click', function(e) {
+			e.preventDefault();
+			$('html, body').animate({scrollTop:0}, '300');
+			});
+
+
 
 	// Main Sections: Two.
 
